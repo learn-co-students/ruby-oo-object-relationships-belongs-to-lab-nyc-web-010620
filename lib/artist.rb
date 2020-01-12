e6497f6a9017d127be9990ed308ad7e955a26cfa
github username <github email address>
2020-01-11 19:01:30 -0500

    Done.

---
+++ lib/artist.rb
@@ -0,0 +1,6 @@
+class Artist
+    #Saw Alex's solutions when helping him do labs thru MVS instead of IDE,
+    #Gonna use it since thats as terse and clear as its gonna get
+    attr_accessor :name
+
+end
